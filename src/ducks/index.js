2c7e8b5ee3d7b944583625edf0d9a7891a7d4382
Michał Przybyła <michalprzybyla@MacBook-Pro-Micha.local>
2018-07-08 18:27:00 +0200

    add profile duck

--- src/ducks/index.js
+++ src/ducks/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import login from './login';
 import places from './places';
 import placeDetails from './placeDetails';
+import addEvent from './addEvent';
 
 const rootReducer = combineReducers({
   login,
   places,
-  placeDetails
+  placeDetails,
+  addEvent
 });
 
 export default rootReducer;
