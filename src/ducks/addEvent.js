6d22abb960f93a1f73f57319961915e51120fe70
Michał Przybyła <michalprzybyla@MacBook-Pro-Micha.local>
2018-07-08 18:26:50 +0200

    add addevent duck

---
+++ src/ducks/addEvent.js
@@ -0,0 +1,44 @@
+// @flow
+import { Map } from 'immutable';
+
+import { createAction, handleActions } from 'redux-actions';
+
+export const actions = {
+  PUT: createAction('ADD_EVENT/PUT'),
+  SUCCESS: createAction('ADD_EVENT/SUCCESS'),
+  ERROR: createAction('ADD_EVENT/ERROR'),
+  INPUT: createAction('ADD_EVENT/INPUT')
+};
+
+type StateType = Map<string, string | boolean | Map<string, any>>;
+
+export type AddEventActionType = {
+  payload: string | number | boolean
+};
+
+export const initialState = Map({
+  adress: '',
+  time: '',
+  game: '',
+  players: '',
+  begginers: false,
+  private: true,
+  isProcessing: false,
+  error: false
+});
+
+const reducer = handleActions(
+  {
+    [actions.SUCCESS]: (state: StateType, action: AddEventActionType) =>
+      state.set('isProcessing', false),
+    [actions.REQUEST]: (state: StateType, action: AddEventActionType) =>
+      state.set('isProcessing', true),
+    [actions.ERROR]: (state: StateType, action: AddEventActionType) =>
+      state.set('isProcessing', false).set('error', true),
+    [actions.INPUT]: (state: StateType, action: AddEventActionType) =>
+      state.set(action.payload[0], action.payload[1])
+  },
+  initialState
+);
+
+export default reducer;
