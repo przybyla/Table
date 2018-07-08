e27d400edbab2c99d776dce417152697e6372bb5
Michał Przybyła <michalprzybyla@MacBook-Pro-Micha.local>
2018-07-08 18:28:25 +0200

    add profile scene

---
+++ src/scenes/Profile/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ProfileWrapper = styled.div`
+  padding: 10px;
+`;
+
+const Profile = () => <ProfileWrapper>Profile</ProfileWrapper>;
+
+export default Profile;
