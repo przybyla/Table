3b2f05da9bf49b0855ad2d684be3128a331e070e
Michał Przybyła <michalprzybyla@macbook-pro-micha.home>
2018-09-19 19:32:17 +0200

    Update scenes routing

--- src/scenes/index.js
+++ src/scenes/index.js
@@ -1,39 +1,63 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
 import { Router, Switch, Route } from 'react-router-dom';
 import history from './history';
+import { apiKey } from '../config';
 import List from './List';
+import EventDetails from './EventDetails';
 import AddEvent from './AddEvent';
-import Menu from '../components/Menu';
-import Header from '../components/Header';
-import Login from './Login';
+import GoogleMapComponent from '../components/GoogleMap';
 
 const AppWrapper = styled.div`
   max-width: 1920px;
   width: 100%;
   margin: 0 auto;
   overflow-x: hidden;
+  display: flex;
+`;
+
+const MapWrapper = styled.div`
+  height: 100vh;
+  width: calc(100vw - 300px);
 `;
 
 class Scenes extends Component {
   state = {};
   render() {
+    const { places } = this.props;
     return (
-      <AppWrapper>
-        <Router history={history}>
-          <div>
-            <Header />
-            <Menu />
-            <Switch>
-              <Route exact path="/" component={List} />
-              <Route exact path="/add-event" component={AddEvent} />
-              <Route exact path="/login" component={Login} />
-            </Switch>
-          </div>
-        </Router>
-      </AppWrapper>
+      <Router history={history}>
+        <AppWrapper>
+          <Route path="/:event?/:name?" component={List} />
+          <Switch>
+            <Route exact path="/event/:name" component={EventDetails} />
+            <Route
+              exact
+              path="/"
+              render={() => (
+                <GoogleMapComponent
+                  isMarkerShown
+                  googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&v=3.exp&libraries=geometry,drawing,places`}
+                  loadingElement={<div style={{ height: `100%` }} />}
+                  containerElement={<MapWrapper />}
+                  mapElement={<div style={{ height: `100%` }} />}
+                  places={places}
+                />
+              )}
+            />
+          </Switch>
+        </AppWrapper>
+      </Router>
     );
   }
 }
 
-export default Scenes;
+const mapStateToProps = (state: any) => ({
+  places: state.places.get('places')
+});
+
+export default connect(
+  mapStateToProps,
+  {}
+)(Scenes);
