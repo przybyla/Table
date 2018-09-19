80aae664b1f896c0311cf0b2c006f72844898879
Michał Przybyła <michalprzybyla@macbook-pro-micha.home>
2018-09-19 19:32:04 +0200

    Add search input for list

---
+++ src/scenes/List/components/SearchInput/SearchInput.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  width: 250px;
+  height: 45px;
+  border-radius: 4px;
+  border: none;
+  box-shadow: 0px 5px 25px -6px rgba(0, 0, 0, 0.75);
+  color: #707070;
+  padding: 5px;
+  box-sizing: border-box;
+  margin: 25px 0 40px 0;
+  &::placeholder {
+    opacity: 0.5;
+    font-size: 10px;
+  }
+  &:focus {
+    outline: 0;
+  }
+`;
+
+const SearchInput = () => <Input placeholder="Enter your city" />;
+
+export default SearchInput;
