125fc065a618b6c45c42e5d0c778c2d3a7673946
Michał Przybyła <michalprzybyla@macbook-pro-micha.home>
2018-09-19 19:30:42 +0200

    Change default center and zoom on map

--- src/components/GoogleMap/GoogleMap.js
+++ src/components/GoogleMap/GoogleMap.js
@@ -8,9 +8,7 @@ import {
 
 const GoogleMapComponent = withScriptjs(
   withGoogleMap(props => (
-    <GoogleMap
-      defaultZoom={13}
-      defaultCenter={{ lat: 50.049683, lng: 19.944544 }}>
+    <GoogleMap defaultZoom={6} defaultCenter={{ lat: 52.0, lng: 20.0 }}>
       {props.isMarkerShown &&
         props.places.map(place => (
           <Marker position={{ lat: place.lat, lng: place.lng }} />
