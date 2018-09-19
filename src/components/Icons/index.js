8c1466e604b5502da3220721491cf5b7de35ef94
Michał Przybyła <michalprzybyla@macbook-pro-micha.home>
2018-09-19 19:30:59 +0200

    Add new icons

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -1,6 +1,27 @@
 // @flow
 import React from 'react';
 
+export const Add = ({ color }: { color: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="30"
+    height="30"
+    viewBox="0 0 24 24">
+    <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" fill="#fff" />
+    <path d="M0 0h24v24H0z" fill="none" />
+  </svg>
+);
+export const Back = ({ color }: { color: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="30"
+    height="30"
+    viewBox="0 0 24 24">
+    <path d="M11.67 3.87L9.9 2.1 0 12l9.9 9.9 1.77-1.77L3.54 12z" fill="#fff" />
+    <path fill="none" d="M0 0h24v24H0z" />
+  </svg>
+);
+
 export const ListIcon = ({ color }: { color: string }) => (
   <svg
     aria-hidden="true"
